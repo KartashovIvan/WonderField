58d257e77296fc8019bce16655db1dc55e2bf7dc
Kartashov Ivan <vano.noname69@yandex.ru>
2023-11-22 19:33:16 +0300

    Выполнен подготовительный этап

---
+++ src/org/javaacadmey/wonder_field/Game.java
@@ -0,0 +1,50 @@
+package org.javaacadmey.wonder_field;
+
+import java.util.Scanner;
+
+public class Game {
+
+    private final int NUMBER_OF_PLAYERS = 3;
+    private final int NUMBER_OF_ROUNDS = 4;
+    private final int NUMBER_OF_GROUP_ROUNDS = 3;
+    private final String[] QUESTION = new String[NUMBER_OF_ROUNDS];
+    private final String[] ANSWERS = new String[NUMBER_OF_ROUNDS];
+    static final Scanner READER = new Scanner(System.in);
+
+    public Game() {
+        System.out.println("Запуск игры \"Поле Чудес\" - подготовка к игре. Вам нужно ввести вопросы и ответы для игры.");
+        QUESTION[0] = "Как называется третья планета от солнца?";
+        QUESTION[1] = "Вопрос 2";
+        QUESTION[2] = "Вопрос 3";
+        QUESTION[3] = "Вопрос 4";
+
+        ANSWERS[0] = "Земля";
+        ANSWERS[1] = "Ответ 2";
+        ANSWERS[2] = "Ответ 3";
+        ANSWERS[3] = "Ответ 4";
+
+        init();
+    }
+
+    public void init() {
+//        System.out.println("Запуск игры \"Поле Чудес\" - подготовка к игре. Вам нужно ввести вопросы и ответы для игры.");
+//        for (int i = 0; i < NUMBER_OF_ROUNDS; i++) {
+//            System.out.println("Введите вопрос #" + (i + 1));
+//            QUESTION[i] = READER.nextLine();
+//            System.out.println("Введите ответ вопрос #" + (i + 1));
+//            ANSWERS[i] = READER.nextLine();
+//        }
+
+        System.out.println("Иницализация закончена, игра начнется через 5 секунд");
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        for (int i = 0; i < 50; i++) {
+            System.out.println();
+        }
+    }
+
+}
---
+++ src/org/javaacadmey/wonder_field/Runner.java
@@ -0,0 +1,7 @@
+package org.javaacadmey.wonder_field;
+
+public class Runner {
+    public static void main(String[] args) {
+        Game game = new Game();
+    }
+}
